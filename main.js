1cba1ba4fcd7800b852f6a83f1bf98918a46e878
Nubar Sariyeva <7nme01l@code.edu.az>
2023-05-22 05:34:44 -0800

    19

---
+++ main.js
@@ -0,0 +1,10 @@
+let card = document.querySelector(".card");
+
+
+axios.get('https://jsonplaceholder.typicode.com/posts').then(response=>{
+    for (let i = 0; i < 4; i++) {
+        card.innerHTML += 
+       
+        
+    }
+})
